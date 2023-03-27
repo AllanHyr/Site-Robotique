391aa2ca823bc544c36f1cee7e5c1f35f024417f
Bougy <djaleial@outlook.fr>
2023-03-27 09:53:46 +0200

    Ajout des pages projets

--- projets.php
+++ projets.php
@@ -8,7 +8,7 @@
     <link rel="icon" type="image/x-icon" href="assets/Logo_transparent.png">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="galeries.css">
+    <link rel="stylesheet" href="projets.css">
     <link rel="stylesheet" href="normalize.css">
     <link rel="stylesheet" href="navbar.css">
     <link rel="stylesheet" href="footer.css">
@@ -19,47 +19,75 @@
         rel="stylesheet">
 </head>
 
-<body class="galCont">
+<body class="projetsCont">
     <?php include('navbar.html'); ?>
-    <main>
-        <div class="titreGal">
-            <h1 class="titreH">Projets</h1>
-        </div>
-        <div class="bigBlockGal">
-            <div class="blockGal">
-                <div class="imageGal"><img class="imgGal" src="assets/mainRobot.avif" alt="test image"></div>
-                <div class="miniBlockGal">
-                    <a href="galerieA.php" class="boutonGal">Galerie</a>
-                    <p class="projT">Projet A</p>
-                    <a class="boutonGal" href="projetA.php">Projet</a>
-                </div>
+    <main class="mainProj">
+        <div class="allContent">
+            <div class="titreProjets">
+                <h1 class="titreH">Projets</h1>
             </div>
-            <div class="blockGal">
-                <div class="imageGal"><img class="imgGal" src="assets/peurRobot.webp" alt="test image"></div>
-                <div class="miniBlockGal">
-                    <a href="#" class="boutonGal">Galerie</a>
-                    <p class="projT">Projet B</p>
-                    <a class="boutonGal" href="#">Projet</a>
+            <div class="titreProjx"><h2>Projet A</h2></div>
+            <div class="imageTexteProjets">
+                <div class="texteProjx">
+                    <div class="textProjx2">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo fugit maiores laudantium fugiat, voluptatem temporibus sapiente velit neque quibusdam, eos aperiam saepe dignissimos minus, reprehenderit dolores. In ullam laboriosam expedita.
+                        Ducimus eaque laudantium adipisci porro deserunt, soluta qui consequatur incidunt. Odio quo facere doloribus aspernatur voluptas quam, eveniet mollitia beatae? Similique hic, dolor sapiente nulla atque maiores nisi excepturi veniam!
+                        Laborum dolorem dignissimos eaque culpa praesentium delectus obcaecati nam, nobis autem officia sequi. Praesentium debitis, facere dolorum, dicta, magnam vero aliquid consequatur quis sint repellendus blanditiis cumque a culpa. Unde?
+                        Beatae consectetur ratione mollitia cumque sint dolor. Sit, ullam. Aliquam eaque nobis quos, impedit saepe omnis minus sint assumenda provident magnam nihil voluptatibus quasi labore quam quae tenetur eius doloremque!
+                        Cum, laboriosam enim minus quis ad nemo quae quasi nobis suscipit amet possimus, mollitia consequuntur? Nam accusantium rem natus molestiae eius, commodi illum facilis officiis. Quod laborum hic exercitationem omnis.</p>
+                    </div>
+                    <div class="bouton">
+                        <form action="projetA.php">
+                            <button class="boutonProj" type="submit">La suite de l'article</button>
+                        </form>
+                        <form action="galerieA.php">
+                            <button class="boutonProj" type="submit">Voir les images</button>
+                        </form>    
+                    </div>
                 </div>
+                <div class="imageProjx"><img class="imgProjx" src="assets/mainRobot.avif" alt="test image"></div>
             </div>
-        </div>
-        <HR class="barreGal" WIDTH="35%">
-        <div class="bigBlockGal">
-            <div class="blockGal">
-                <div class="imageGal"><img class="imgGal" src="assets/reflexionRobot.jpg" alt="test image"></div>
-                <div class="miniBlockGal">
-                    <a href="#" class="boutonGal">Galerie</a>
-                    <p class="projT">Projet C</p>
-                    <a class="boutonGal" href="#">Projet</a>
-                </div>
+            <div class="titreProjx"><h2>Projet B</h2></div>
+            <div class="imageTexteProjets">
+                <div class="texteProjx">
+                    <div class="textProjx2">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque quasi odit hic quos sequi. At, aspernatur deserunt a, animi doloribus nemo molestiae, atque quo maiores vero nesciunt sequi provident cupiditate.
+                        Quae hic sed accusantium quis labore molestias nobis blanditiis vero, non quaerat cum ab quia minus sunt harum maxime. Repellendus, ab dolor enim ea neque autem iure aut odio dolore?
+                        Facere quia dolores in molestias eius. Perferendis, praesentium. Voluptatibus iure fugit facilis expedita et rerum saepe, unde quae aperiam magni, assumenda quo numquam necessitatibus impedit inventore, quasi sapiente illo ea.
+                        Minima facere sed labore saepe alias corrupti voluptatem dicta amet repellat laudantium, nihil pariatur inventore cumque, iste, rem libero dolorem perspiciatis placeat aut illum? Quas deleniti nemo similique blanditiis adipisci.
+                        Iure eveniet dolor, quos eius tempore iste possimus ab a excepturi, nostrum neque perferendis veritatis, explicabo recusandae aspernatur consequatur unde quibusdam autem asperiores dignissimos debitis? Non aliquid facere dicta voluptas.</p>
+                    </div>
+                    <div class="bouton">
+                        <form action="projetA.php">
+                            <button class="boutonProj" type="submit">La suite de l'article</button>
+                        </form>
+                        <form action="galerieA.php">
+                            <button class="boutonProj" type="submit">Voir les images</button>
+                        </form>    
+                    </div>
             </div>
-            <div class="blockGal">
-                <div class="imageGal"><img class="imgGal" src="assets/heureuxRobot.jpeg" alt="test image"></div>
-                <div class="miniBlockGal">
-                    <a href="#" class="boutonGal">Galerie</a>
-                    <p class="projT">Projet D</p>
-                    <a class="boutonGal" href="#">Projet</a>
+                <div class="imageProjx"><img class="imgProjx" src="assets/peurRobot.webp" alt="test image"></div>
+            </div>
+            <div class="titreProjx"><h2>Projet C</h2></div>
+            <div class="imageTexteProjets">
+                <div class="texteProjx">
+                    <div class="textProjx2"> 
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum incidunt repudiandae fugit quam facere molestiae, ducimus ullam? Quos corporis accusantium, cupiditate aperiam sequi, illo nulla sapiente quaerat quas tempore officiis?
+                        Placeat aperiam ullam neque iste inventore ad amet dolores, ipsam ipsa sapiente! Voluptatem cum temporibus corporis, ipsa, vero ducimus in inventore dolorem repudiandae excepturi incidunt est, rerum sapiente atque perspiciatis.
+                        Iste asperiores omnis quo ipsam qui voluptatem similique aliquam deleniti voluptatum. Laborum nihil officia cum sit, non odit mollitia, officiis ullam quibusdam facilis eaque delectus porro dolorum voluptate aperiam repellendus!
+                        Quis deserunt iure magni eum ratione molestiae repellendus voluptate animi assumenda consequuntur quae fuga officiis sit soluta quo odio, suscipit laboriosam asperiores reiciendis delectus dolore possimus! Sed repellat consequuntur nobis.
+                        Non sequi molestiae iusto explicabo quaerat dolor, velit nisi hic facilis unde modi doloremque ex quasi est consectetur sed totam soluta consequuntur omnis recusandae nobis repudiandae odit magni eveniet. Repellat.</p>
+                    </div>
+                    <div class="bouton">
+                        <form action="projetA.php">
+                            <button class="boutonProj" type="submit">La suite de l'article</button>
+                        </form>
+                        <form action="galerieA.php">
+                            <button class="boutonProj" type="submit">Voir les images</button>
+                        </form>    
+                    </div>
                 </div>
+                <div class="imageProjx"><img class="imgProjx" src="assets/reflexionRobot.jpg" alt="test image"></div>
             </div>
         </div>
     </main>
---
+++ projetsTest.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="projetsTest.css">
+    <link rel="stylesheet" href="normalize.css">
+    <link rel="stylesheet" href="navbar.css">
+    <link rel="stylesheet" href="footer.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,400;0,600;1,400&family=Roboto&family=Work+Sans&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <?php include('navbar.html'); ?>
+    <main>
+        <div class="titreProjets">
+                <h1 class="titreH">Projets</h1>
+        </div>
+        <div class="barreLong">
+            <div class="boxProjets">
+                <h2>Projets A</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque, autem aperiam aspernatur quisquam labore recusandae delectus officiis, unde, esse ullam incidunt perspiciatis sequi atque tempore mollitia ab. Consequuntur, tenetur quasi?</p>
+                <div class="allButton">
+                    <button>La suite de l'article</button>
+                    <button>Voir les images</button>
+                </div>
+            </div>
+            <div class="boxProjets">
+                <h2>Projets B</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque, autem aperiam aspernatur quisquam labore recusandae delectus officiis, unde, esse ullam incidunt perspiciatis sequi atque tempore mollitia ab. Consequuntur, tenetur quasi?</p>
+                <div class="allButton">
+                    <button>La suite de l'article</button>
+                    <button>Voir les images</button>
+                </div> 
+            </div>
+        </div>
+
+        <div class="barreLong">
+            <div class="boxProjets">
+                <h2>Projets C</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque, autem aperiam aspernatur quisquam labore recusandae delectus officiis, unde, esse ullam incidunt perspiciatis sequi atque tempore mollitia ab. Consequuntur, tenetur quasi?</p>
+                <div class="allButton">
+                    <button>La suite de l'article</button>
+                    <button>Voir les images</button>
+                </div> 
+            </div>
+            <div class="boxProjets">
+                <h2>Projets D</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque, autem aperiam aspernatur quisquam labore recusandae delectus officiis, unde, esse ullam incidunt perspiciatis sequi atque tempore mollitia ab. Consequuntur, tenetur quasi?</p>
+                <div class="allButton">
+                    <button>La suite de l'article</button>
+                    <button>Voir les images</button>
+                </div> 
+            </div>
+        </div>
+    </main>
+    <?php include('footer.html'); ?>
+</body>
+</html>
