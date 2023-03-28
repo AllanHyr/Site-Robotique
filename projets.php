5cecdca74d34fb852fafd2ea2e2aa30bfa5b21e2
FlorianB <benoitonflorian@gmail.com>
2023-03-28 18:14:18 +0200

    Commentaire projets

--- projets.php
+++ projets.php
@@ -4,7 +4,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Galerie - Club Robotique</title>
+    <title>Projet - Club Robotique</title>
     <link rel="icon" type="image/x-icon" href="assets/Logo_transparent.png">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -23,19 +23,24 @@
     <?php include('navbar.html'); ?>
     <main class="mainProj">
         <div class="allContent">
+            <!-- titre principale -->
             <div class="titreProjets">
                 <h1 class="titreH">Projets</h1>
             </div>
+            <!-- titre du projet -->
             <div class="titreProjx"><h2>Projet Claptrap</h2></div>
+            <!-- contient les images et textes du projet -->
             <div class="imageTexteProjets">
                 <div class="texteProjx">
+                    <!-- texte du projet -->
                     <div class="textProjx2">
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo fugit maiores laudantium fugiat, voluptatem temporibus sapiente velit neque quibusdam, eos aperiam saepe dignissimos minus, reprehenderit dolores. In ullam laboriosam expedita.
-                        Ducimus eaque laudantium adipisci porro deserunt, soluta qui consequatur incidunt. Odio quo facere doloribus aspernatur voluptas quam, eveniet mollitia beatae? Similique hic, dolor sapiente nulla atque maiores nisi excepturi veniam!
+                        Ducimus eaque laudantium adipisci porro deserunt, soluta qui consequatur incidunt. Odio quo facere doloribus aspernatur voluptas quam, eveniet mollitia beatae? Similique hic, dolor sapiente nulla atque maiores nisi excepturi veniam!projets.php
                         Laborum dolorem dignissimos eaque culpa praesentium delectus obcaecati nam, nobis autem officia sequi. Praesentium debitis, facere dolorum, dicta, magnam vero aliquid consequatur quis sint repellendus blanditiis cumque a culpa. Unde?
                         Beatae consectetur ratione mollitia cumque sint dolor. Sit, ullam. Aliquam eaque nobis quos, impedit saepe omnis minus sint assumenda provident magnam nihil voluptatibus quasi labore quam quae tenetur eius doloremque!
                         Cum, laboriosam enim minus quis ad nemo quae quasi nobis suscipit amet possimus, mollitia consequuntur? Nam accusantium rem natus molestiae eius, commodi illum facilis officiis. Quod laborum hic exercitationem omnis.</p>
                     </div>
+                    <!-- div qui contient les boutons -->
                     <div class="bouton">
                         <form action="projetA.php">
                             <button class="boutonProj" type="submit">La suite de l'article</button>
@@ -48,8 +53,10 @@
                 <div class="imageProjx"><img class="imgProjx" src="assets/galerieA/claptrap.png" alt="Claptrap qui nous pointe avec une Fatale de Borderlands"></div>
             </div>
             <div class="titreProjx"><h2>Projet Jeu arcade</h2></div>
+            <!-- contient les images et textes du projet -->
             <div class="imageTexteProjets">
                 <div class="texteProjx">
+                    <!-- texte du projet -->
                     <div class="textProjx2">
                         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque quasi odit hic quos sequi. At, aspernatur deserunt a, animi doloribus nemo molestiae, atque quo maiores vero nesciunt sequi provident cupiditate.
                         Quae hic sed accusantium quis labore molestias nobis blanditiis vero, non quaerat cum ab quia minus sunt harum maxime. Repellendus, ab dolor enim ea neque autem iure aut odio dolore?
@@ -57,6 +64,7 @@
                         Minima facere sed labore saepe alias corrupti voluptatem dicta amet repellat laudantium, nihil pariatur inventore cumque, iste, rem libero dolorem perspiciatis placeat aut illum? Quas deleniti nemo similique blanditiis adipisci.
                         Iure eveniet dolor, quos eius tempore iste possimus ab a excepturi, nostrum neque perferendis veritatis, explicabo recusandae aspernatur consequatur unde quibusdam autem asperiores dignissimos debitis? Non aliquid facere dicta voluptas.</p>
                     </div>
+                    <!-- div qui contient les boutons -->
                     <div class="bouton">
                         <form action="projetA.php">
                             <button class="boutonProj" type="submit">La suite de l'article</button>
@@ -69,8 +77,10 @@
                 <div class="imageProjx"><img class="imgProjx" src="assets/jeuArcade/reflexion.jpg" alt="Une équipe qui refléchit"></div>
             </div>
             <div class="titreProjx"><h2>Projet Sumobot</h2></div>
+            <!-- contient les images et textes du projet -->
             <div class="imageTexteProjets">
                 <div class="texteProjx">
+                    <!-- texte du projet -->
                     <div class="textProjx2"> 
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum incidunt repudiandae fugit quam facere molestiae, ducimus ullam? Quos corporis accusantium, cupiditate aperiam sequi, illo nulla sapiente quaerat quas tempore officiis?
                         Placeat aperiam ullam neque iste inventore ad amet dolores, ipsam ipsa sapiente! Voluptatem cum temporibus corporis, ipsa, vero ducimus in inventore dolorem repudiandae excepturi incidunt est, rerum sapiente atque perspiciatis.
@@ -78,6 +88,7 @@
                         Quis deserunt iure magni eum ratione molestiae repellendus voluptate animi assumenda consequuntur quae fuga officiis sit soluta quo odio, suscipit laboriosam asperiores reiciendis delectus dolore possimus! Sed repellat consequuntur nobis.
                         Non sequi molestiae iusto explicabo quaerat dolor, velit nisi hic facilis unde modi doloremque ex quasi est consectetur sed totam soluta consequuntur omnis recusandae nobis repudiandae odit magni eveniet. Repellat.</p>
                     </div>
+                    <!-- div qui contient les boutons -->
                     <div class="bouton">
                         <form action="projetA.php">
                             <button class="boutonProj" type="submit">La suite de l'article</button>
@@ -90,8 +101,10 @@
                 <div class="imageProjx"><img class="imgProjx" src="./assets/sumobot/sumobot_trois.jpg" alt="Photo des 3 sumobot construit lors de la semaine transverse"></div>
             </div>
             <div class="titreProjx"><h2>Atelier Soudure</h2></div>
+            <!-- contient les images et textes du projet -->
             <div class="imageTexteProjets">
                 <div class="texteProjx">
+                    <!-- texte du projet -->
                     <div class="textProjx2">
                         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque quasi odit hic quos sequi. At, aspernatur deserunt a, animi doloribus nemo molestiae, atque quo maiores vero nesciunt sequi provident cupiditate.
                         Quae hic sed accusantium quis labore molestias nobis blanditiis vero, non quaerat cum ab quia minus sunt harum maxime. Repellendus, ab dolor enim ea neque autem iure aut odio dolore?
@@ -99,6 +112,7 @@
                         Minima facere sed labore saepe alias corrupti voluptatem dicta amet repellat laudantium, nihil pariatur inventore cumque, iste, rem libero dolorem perspiciatis placeat aut illum? Quas deleniti nemo similique blanditiis adipisci.
                         Iure eveniet dolor, quos eius tempore iste possimus ab a excepturi, nostrum neque perferendis veritatis, explicabo recusandae aspernatur consequatur unde quibusdam autem asperiores dignissimos debitis? Non aliquid facere dicta voluptas.</p>
                     </div>
+                    <!-- div qui contient les boutons -->
                     <div class="bouton">
                         <form action="projetA.php">
                             <button class="boutonProj" type="submit">La suite de l'article</button>
